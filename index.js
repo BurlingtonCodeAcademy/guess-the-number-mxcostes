5b896e9dc1f03d0e44b28813ba7cdce5a640c4ad
Max Costes <maxcostes@Maxs-MacBook.local>
2020-02-07 12:34:26 -0500

    worked up to optimization

--- index.js
+++ index.js
@@ -1,69 +1,135 @@
 const readline = require('readline');
+
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+ 
+
 function ask(questionText) {
-	return new Promise((resolve, reject) => {
-		rl.question(questionText, resolve);
-	});
+
+  return new Promise((resolve, reject) => {
+
+    rl.question(questionText, resolve);
+
+  });
+
 }
+
+ 
+
+async function guessFcn(maxGuess, minGuess) {
+
+    const maxNumber = Number.parseFloat((maxGuess - 1))
+
+    const minNumber = Number.parseFloat(minGuess)
+
+    const difference = (maxNumber - minNumber);
+
+    const random = Math.random();
+
+    const product = Number.parseFloat((random * difference) + 1);
+
+//alternative for optimal solution
+
+   // const = rawGuess = (Number())
+
+//used for random guess
+
+    const rawGuess = (Number(product)) + Number(minGuess);
+
+    console.log(difference)
+
+    console.log(random)
+
+    console.log(product)
+
+    console.log(rawGuess)
+
+    console.log(minNumber)
+
+    console.log(maxGuess)
+
+  let guess = Math.floor(rawGuess); 
+
+  let response = await ask('Is it ' + guess + '? (yes or no)  ');
+
+  //sanitize response
+
+  let responseSan = response.toLowerCase().trim();
+
+  if (responseSan === 'yes') {
+
+    console.log('Conratulations you have guessed the number.');
+
+    process.exit();
+
+  }
+
+  if (responseSan === 'no') {
+
+    await ifNo(guess, maxNumber, minNumber);
+
+  }
+
+}
+
+ 
+
+async function ifNo(wrongGuess, max, min) {
+
+  let adjustment = await ask('Is it higher or lower? (higher or lower)  ');
+
+  //sanitize response
+
+  let adjustmentSan = adjustment.toLowerCase().trim();
+
+  if (adjustmentSan === 'higher') {
+
+    await guessFcn(max, wrongGuess)
+
+    //higher(guess);
+
+  } else if (adjustmentSan === 'lower') {
+
+    await guessFcn(wrongGuess, min)
+
+    //lower(guess);
+
+  }
+
+}
+
+ 
 
 start();
 
+ 
+
 async function start() {
-	console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
-	let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-	console.log('You entered: ' + secretNumber);
-	//script after starting (story : pick a number any number)
-	//Set the range by assigning max and min guesses
-	let max = await ask('What is the highest the number may be? ');
-	let min = await ask('And what may the lowest number be? ');
-	// Now try and complete the program.
-	//The computer guesses
-	let guess = Math.floor(Math.random() * (max - min) + min);
-	let response = await ask('Is it ' + guess + '? (yes or no)  ');
-	//sanitize response
-	let responseSan = response.toLowerCase().trim();
-	if (responseSan === 'yes') {
-		console.log('Conratulations you have guessed the number.');
-		process.exit();
-	}
-	if (responseSan === 'no') {
-		let adjustment = await ask('Is it higher or lower? (higher or lower)  ');
-		//sanitize response
-		let adjustmentSan = adjustment.toLowerCase().trim();
-		if (adjustmentSan === 'higher') {
-			higher(guess);
-		} else if (adjustmentSan === 'lower') {
-			lower(guess);
-		}
-	}
 
-	//Funcitons
-	//Function to change ange to lower ceiling
+  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
 
-	async function lower(num) {
-		// New Guess assigning random number with lower ceiling.
-		let guessLower = Math.floor(Math.random() * (guess - min + 1) + min);
-		let lowerGuess = await ask('Is it ' + guessLower + '(yes or no)   ');
-		if (lowerGuess === 'no') {
-			//loop to no funciton
-		}
-		if (lowerGuess === 'yes') {
-			//loop to yes funciton
-		}
-	}
+  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
 
-	async function higher(num) {
-		// New guess assessing random number with new floor. STILL NEEDS WORK
-		let guessHigher = Math.floor(Math.random() * (max - guess + 1) + guess);
-		let higherGuess = await ask('Is it ' + guessHigher + '(yes or no)   ');
-		if (higherGuess === 'yes') {
-			//input yes funciton
-		}
-		if (higherGuess === 'no') {
-			// input no funciton
-		}
-	}
+  console.log('You entered: ' + secretNumber);
 
-	process.exit();
+  //script after starting (story : pick a number any number)
+
+  //Set the range by assigning max and min guesses
+
+  const max = await ask('What is the highest the number may be? ');
+
+  const min = await ask('And what may the lowest number be? ');
+
+  // Now try and complete the program.
+
+  //The computer guesses
+
+  
+
+  await guessFcn(max, min)
+
+ 
+
+  process.exit();
+
 }
