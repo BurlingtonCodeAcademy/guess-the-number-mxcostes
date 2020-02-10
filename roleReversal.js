67082907f756f59fe9c4d3e65edf11a6980526ae
Max-Costes <mxcostes@gmail.com>
2020-02-09 20:09:01 -0500

    Added minimun value set in roleReveral.js as well as comments

--- roleReversal.js
+++ roleReversal.js
@@ -8,17 +8,19 @@ function ask(questionText) {
         rl.question(questionText, resolve);
     });
 }
-
+// human enters guess. 
 async function guessMaker(secretNumber) {
     const guessDirty = await ask('What is your guess? ');
 
     const guess = Number.parseFloat(guessDirty);
 
+    //if guess is correct you are congratulated and the program is exited
     if (guess === secretNumber) {
         console.log('You got it! Not bad for a human.');
 
         process.exit();
     }
+    //is the guess is incorrect you are sent to receive the computers response
     if (guess !== secretNumber) response(guess, secretNumber);
     newGuessDirty = await ask('What is your new guess?');
 
@@ -28,6 +30,7 @@ async function guessMaker(secretNumber) {
 }
 
 function response(guess, secretNumber) {
+    //feedback given to the human guesser that sends sends you back to the guessing stage.
     if (guess > secretNumber) {
         console.log('Your guess is too high. \n Try again. ');
 
@@ -48,15 +51,29 @@ async function start() {
         "Lets play a game where I (computer) think of a number\nAnd you (human) have to guess what number I'm thinking of. "
     );
 
-    const max = await ask('What is the highest my number may be? ');
+    const maxDirty = await ask('What is the highest my number may be? ');
 
-    console.log('Alright then the number will be between 0 and ' + max);
+    const max = Number.parseFloat(maxDirty)
 
-    const startingNumber = Math.floor(Math.random() * max);
+    const minDirty = await ask('What is the lowest my number may be? ')    
 
+    const min = Number.parseFloat(minDirty)
+
+    console.log('Alright then the number will be between ' + min +   ' and ' + max);
+
+    const difference = Number.parseFloat(max - min)
+
+    const startingNumber = ((Math.floor(Math.random() * difference) + min));
+    console.log(startingNumber)
+    console.log(difference)
+        // the computer selects and sanitizes a random number between 0 and the 
     const secretNumber = Number.parseFloat(startingNumber);
+console.log(secretNumber)
+console.log(max)
+console.log(min)
 
+    //a guess is given
     await guessMaker(secretNumber);
-
+    //you receive feedbacck based on your guess
     response(guess, secretNumber);
 }
